48c07d866ac6479107f5d4b6603a4f4f84861a7b
Jonas Van Pelt <jonas.van.pelt@gmail.com>
2015-11-22 16:24:27 +0100

    added js file

---
+++ index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+		$("#colorHex").spectrum({
+			preferredFormat : "hex",
+			color : "#0000ff",
+			flat : true,
+			showInput : true,
+			showButtons : false,
+		}).on("dragstop.spectrum", function(e, color) {
+			$.ajax({
+				type : "GET",
+				url : "setColor.esp",
+				dataType : "xml",
+				data : ( {
+					'colorHex' : color.toHexString()
+				}),
+				success : function(response) {
+					
+					 var ci_status = ($(response).find('ci').first().text() === "true");
+					 console.log(ci_status);
+					
+					if(ci_status == false)
+					{
+						$("#pico_logo").attr("src","https://rawgit.com/jonasvanpelt/barlights_files/master/broken.png");
+						$("form").remove();
+						$("#feedback").show();
+					}
+				}
+			});
+		});
+	});
+
